f31a7b7deed7aa8fcca4bc6a9adea43664ad9bf7
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-09-22 16:03:46 -0500

    This commit adds more functionality to api request

--- js/movie_project.js
+++ js/movie_project.js
@@ -2,10 +2,13 @@
 
 
 const apiURL = 'https://hexagonal-crystal-bit.glitch.me/movies'
-
+// Api request for Json movie objects/////////////
 const getMovies = () => fetch(apiURL)
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(movies => {
+        for(let movie of movies);
+        `<p> ${movies.title} <br> ${movies.rating} <br> ${movies.id} </p> `
+    })
     .catch(error => console.log(error));
-
 console.log(getMovies())
+////////////////////////////////////////
