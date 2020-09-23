919196d9f281423e5e8630a334cef640c739c6bb
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-09-22 21:33:55 -0500

    Fixed for Each Loop in API Promise

--- js/movie_project.js
+++ js/movie_project.js
@@ -5,12 +5,11 @@ const apiURL = 'https://natural-nine-macaroni.glitch.me/movies'
 // Api request for Json movie objects/////////////
 const getMovies = () => fetch(apiURL)
     .then(response => response.json())
-
     .then(movies => {
         movies.forEach(({title, rating, id}) => {
             console.log(`id#${id} - ${title} - rating: ${rating}`)
             // `<p> ${movies.title} <br> ${movies.rating} <br> ${movies.id} </p> `
-        })
+             })
          })
 
 console.log(getMovies())
