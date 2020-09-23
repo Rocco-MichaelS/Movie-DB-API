29d9d40428ec595230bcb38db58782c9e146a52d
Rocco Paccione <leonardo.rocco.paccione@gmail.com>
2020-09-23 12:07:47 -0500

    This commit adds functionality to the data fetch

--- js/movie_project.js
+++ js/movie_project.js
@@ -12,8 +12,67 @@ const getMovies = () => fetch(apiURL)
             divCreate.innerHTML = `id#${id} - ${title} - rating: ${rating}`
             htmlLoad.appendChild(divCreate)
              })
-    .catch(err => console.log('Error Loading Page!'))
+    // .catch(err => console.log('Error Loading Page!'))
     })
 
 console.log(getMovies())
 ////////////////////////////////////////
+let userInput = document.getElementById('addMovie').innerHTML.valueOf();
+const clickMovie = (userInput) =>
+    console.log(userInput)
+    document.getElementById('btnSearch').addEventListener('click', )
+console.log(clickMovie)
+
+const url = 'http://www.omdbapi.com/?apikey='+ omdbAPIKEY + '&s='
+const omdbDATA = () => fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
+
+console.log(omdbDATA())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// const addMovie = {
+//     restaurant_id: 1,
+//     name: 'Codey',
+//     rating: 5,
+//     comments: "This is a really good place for coding and eating"
+// };
+// const options = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(addMovie),
+// };
+// fetch(apiURL, options)
+//     .then( response => console.log(response) ) /* review was created successfully */
+//     .catch( error => console.error(error) ); /* handle errors */
