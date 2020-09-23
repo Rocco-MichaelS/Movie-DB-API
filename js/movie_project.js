2b93caff2e37c1e87a167467693ad2b897344761
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-09-23 08:51:41 -0500

    Added functionality to display api on index page

--- js/movie_project.js
+++ js/movie_project.js
@@ -1,16 +1,19 @@
 
 
-
+const htmlLoad = document.getElementById('movieList');
 const apiURL = 'https://natural-nine-macaroni.glitch.me/movies'
 // Api request for Json movie objects/////////////
 const getMovies = () => fetch(apiURL)
     .then(response => response.json())
     .then(movies => {
         movies.forEach(({title, rating, id}) => {
+            let divCreate = document.createElement("div")
             console.log(`id#${id} - ${title} - rating: ${rating}`)
-            // `<p> ${movies.title} <br> ${movies.rating} <br> ${movies.id} </p> `
+            divCreate.innerHTML = `id#${id} - ${title} - rating: ${rating}`
+            htmlLoad.appendChild(divCreate)
              })
-         })
+    .catch(err => console.log('Error Loading Page!'))
+    })
 
 console.log(getMovies())
 ////////////////////////////////////////
