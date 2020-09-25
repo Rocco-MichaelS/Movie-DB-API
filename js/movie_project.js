1e7570d7d8dc874634ad1de8d787bf2a7560532f
Michael Satterfield <michael.f.satterfield@gmail.com>
2020-09-25 14:03:29 -0500

    Fixed delete button with Jquery implementaion of $().data() function that stores and pulls info from blocked scoped{create and delete function} on line 9

--- js/movie_project.js
+++ js/movie_project.js
@@ -6,30 +6,51 @@ const apiURL = 'https://natural-nine-macaroni.glitch.me/movies'
 const getMovies = () => fetch(apiURL)
     .then(response => response.json())
     .then(movies => {
-        movies.forEach(({title, rating, id}) => {
+            movies.forEach(({title, rating, id}) => {
             let divCreate = document.createElement("div")
-            console.log(`id#${id} - ${title} - rating: ${rating}`)
-            divCreate.innerHTML = `${title} - rating: ${rating}`
+            console.log(` ${title} - rating: ${rating}`)
+            divCreate.innerHTML = `${title} - rating: ${rating}
+                                     <button onclick="Delete(this)" 
+                                     data-rmv=${id}>Remove this Title</button>`
+
             htmlLoad.appendChild(divCreate)
+            divCreate.addEventListener("click", divCreate.remove)
              })
-    // .catch(err => console.log('Error Loading Page!'))
+
+
     })
+
 
 console.log(getMovies())
 ////////////////////////////////////////
 //Add Movie Function***********
 
-const addMovie = () =>{
+const addMovie = () => {
     let stars = document.getElementById('movieRating').value;
     let userInput = document.getElementById('addMovie').value;
     console.log(userInput)
     let createInput = document.createElement("div")
-    createInput.innerHTML = `${userInput} - rating: ${stars} `
+    createInput.innerHTML = `${userInput} - rating: ${stars}  
+                            <button onclick="Delete(this)" 
+                            id = "movieDel">delete</button>`
     htmlLoad.appendChild(createInput)
+    createInput.addEventListener("click", createInput.remove)
+    createInput.addEventListener("click", Delete)
+
+    //OMDB Movie Data********************
+    const url = 'http://www.omdbapi.com/?apikey='+omdbAPIKEY+'&s='+userInput+''
+    const omdbDATA = () => fetch(url)
+        .then(response => response.json())
+
+                .then(data => console.log(data))
+                .catch(error => console.log(error))
+
+            console.log(omdbDATA())
 
 //*****ADD to Glitch JSON Objects***********
 
     let addTitle = {
+        id: '',
         title: userInput,
         rating: stars
     };
@@ -44,51 +65,21 @@ const addMovie = () =>{
         .then( response => console.log(response) ) /* review was created successfully */
         .catch( error => console.error(error) );
 
-}
-//*********Remove Object Function*************//
-const removeMovie = () => {
-    let userRemove = document.getElementById('deleteMovie').value
-    let selectDelete = document.querySelector('#movieList')
-    console.log(selectDelete)
+
 }
 
+const Delete = (id) => {
+ let movieId = $(id).data('rmv')
+    fetch(`${apiURL}/${movieId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        .then(response => response)
+        .then(() => {
+            console.log(`deleted successfully`)
+        })
+        .catch(error => console.error('didnt work'));
 
-
-
-
-const url = 'http://www.omdbapi.com/?apikey='+ omdbAPIKEY + '&s='
-const omdbDATA = () => fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error))
-
-console.log(omdbDATA())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* handle errors */
+}
